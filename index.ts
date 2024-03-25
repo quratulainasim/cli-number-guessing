671e255c7eadf5232de0c7b29632fc07f64ab807
quratulainasim <quratulainasim1234@gmail.com>
2024-03-24 23:44:27 -0700

    create cli-number-guessing-game

---
+++ index.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+console.log("\n\t Wellcome to - MY CLI-NUMBER GUESSING-GAME\n");
+
+const randomNumber =Math.floor(Math.random() *6+1);
+
+const answers=await inquirer.prompt([
+
+
+ { 
+     name: "userGuessedNumber",
+     type: "number",
+ message: "Please guess a number between 1-6:",
+
+ },
+
+
+ ]);
+ if (answers.userGuessedNumber===randomNumber) {
+     console.log ("congratulation!you guessed a right number");
+ }
+ else{
+     console.log ("you guessed wrong number");
+}
